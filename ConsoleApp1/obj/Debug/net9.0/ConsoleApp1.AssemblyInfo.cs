1ad9d5e0907f59fc4e01898ff26fd7ba63305504
Andrii Kunitsyn <akunitsyn@eyeotmonitor.com>
2025-06-22 13:59:53 +0200

    0.0.2

--- ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
+++ ConsoleApp1/obj/Debug/net9.0/ConsoleApp1.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e166009e713b8763cf6c2978362c23190a9d418b")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0a467304397028a5360f06f739692cb93a86137")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp1")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
